9cd7fbb760ac442ca9cf2488829d0c9e479c066c
Isabella <avaldivia@fi.uba.ar>
2023-11-06 21:36:26 -0300

    probando commit

--- src/main/java/edu/fiuba/algo3/Modelo/Message.java
+++ src/main/java/edu/fiuba/algo3/Modelo/Message.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.Modelo;
 
 public class Message extends Localize {
 
--- src/main/test/edu/fiuba/algo3/entrega_1/MessageTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/MessageTest.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.Message;
+import edu.fiuba.algo3.Modelo.Message;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
