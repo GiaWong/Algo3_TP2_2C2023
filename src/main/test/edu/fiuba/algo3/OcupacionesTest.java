3de6f3b591e9cfdfa98c31280a027a0a9026ac3d
Isabella <avaldivia@fi.uba.ar>
2023-11-21 15:26:47 -0300

    Agregué OcupacionesTests pero aun fallan

---
+++ src/main/test/edu/fiuba/algo3/OcupacionesTest.java
@@ -0,0 +1,415 @@
+package edu.fiuba.algo3;
+
+import edu.fiuba.algo3.Modelo.Casillas.Casilla;
+import edu.fiuba.algo3.Modelo.Casillas.NadaOcupacion;
+import edu.fiuba.algo3.Modelo.Dado;
+import edu.fiuba.algo3.Modelo.Equipamientos.Armadura;
+import edu.fiuba.algo3.Modelo.Equipamientos.Casco;
+import edu.fiuba.algo3.Modelo.Equipamientos.EscudoYEspada;
+import edu.fiuba.algo3.Modelo.Equipamientos.LLave;
+import edu.fiuba.algo3.Modelo.Gladiador;
+import edu.fiuba.algo3.Modelo.Premios.Comida;
+import edu.fiuba.algo3.Modelo.Seniority.Novato;
+import edu.fiuba.algo3.Modelo.Tablero;
+import edu.fiuba.algo3.Modelo.Turno;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OcupacionesTest {
+
+    @Test
+    public void Test01LaListaDeCasillasNoTieneEquipamientosNiPremios() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 0;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test02LaListaDeCasillasTieneUnPremioYGladiadorObtieneUnCasco() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 1;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test03LaListaDeCasillasTieneDosPremiosYGladiadorObtieneUnCascoYUnaArmadura() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 2;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+
+    @Test
+    public void Test04LaListaDeCasillasTieneTresPremiosYGladiadorObtieneUnCascoUnaArmaduraUnEscudoEspada() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 3;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test05LaListaDeCasillasTieneCuatroPremiosYGladiadorObtieneLaLLave() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 4;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+
+
+    }
+    @Test
+    public void Test06LaListaDeCasillasTieneUnCascoYGladiadorLoGuardaEnElEquipamiento() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 1;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test07LaListaDeCasillasTieneDosEquipamientos() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new Armadura(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 2;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+
+    }
+    @Test
+    public void Test08LaListaDeCasillasTieneTresEquipamientos() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new Armadura(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new EscudoYEspada(2)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 3;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+
+    }
+    @Test
+    public void Test09LaListaDeCasillasTieneCuatroEquipamientosIncluidoLaLLave() {
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new Armadura(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new EscudoYEspada(2)));
+        tablero.agregarCasilla(new Casilla(new LLave()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+
+        int longitudEsperada = 4;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test10LaListaDeCasillasTieneUnCascoYUnPremioYGladiadorGuardaDosEquipamientos() {
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 2;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test11LaListaDeCasillasTieneUnCascoYDosPremiosYGladiadorGuardaTresEquipamientos() {
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 3;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test12LaListaDeCasillasTieneUnCascoYTresPremiosYGladiadorGuardaCuatroEquipamientos() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 4;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test13LaListaDeCasillasTieneUnPremioYUnCascoYGladiadorGuardaUnEquipamiento() {
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Casco(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 1;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test14LaListaDeCasillasTieneDosPremiosYUnaArmaduraYGladiadorGuardaDosEquipamientos() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new Armadura(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 2;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+    @Test
+    public void Test15LaListaDeCasillasTieneTresPremiosYUnaArmaduraYGladiadorGuardaTresEquipamientos() {
+
+        Tablero tablero = new Tablero(1, new Turno(30));
+
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new Comida(15)));
+        tablero.agregarCasilla(new Casilla(new Armadura(5)));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+        tablero.agregarCasilla(new Casilla(new NadaOcupacion()));
+
+        Gladiador unGladiador = new Gladiador(20,new Novato(),0);
+        tablero.agregarJugador(unGladiador);
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+        tablero.avanzar(new Dado());
+
+        int longitudEsperada = 3;
+        assertEquals(longitudEsperada, unGladiador.obtenerCantidadDeEquipamiento());
+    }
+
+
+
+}
