509a29bf909de1b3618aa675cea7ea961240f224
Roy Fiorilo <rfiorilo@fi.uba.ar>
2023-11-23 13:17:56 -0300

    se mejoro el codigo del tablero

--- src/main/java/edu/fiuba/algo3/Modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/Modelo/Tablero.java
@@ -53,50 +53,41 @@ public class Tablero {
     }
 
     public void avanzar(Dado dado) {
-
-        int cantidadAMoverse = dado.tirar();
         Gladiador ungladiador = turno.siguienteTurno(listaDeGladiadores);
 
-        if (this.validarTurno(ungladiador)) {//refactorizar
+        if (this.validarTurno(ungladiador)) {
             ungladiador.aumentarEnergiaAlIniciarElTurno();
 
-            if(hayMasCasillas()) {
-
+            if (hayMasCasillas()) {
+                int cantidadAMoverse = dado.tirar();
                 ungladiador.avanzar(cantidadAMoverse);
-                //Patron visitor
+
+                // Patron visitor
                 VisitorDeCasillas visitor = new OperacionVisitorDeCasillas();
-                controladorCantidadMoverse = controladorCantidadMoverse + cantidadAMoverse;
+                controladorCantidadMoverse += cantidadAMoverse;
+
                 Casilla casillaActual = listaDeCasillas.get(controladorCantidadMoverse);
                 ungladiador = casillaActual.interactuarConLaOcupacion(visitor, ungladiador);
 
-            }
-
-            if(esLaUltimaPosicion(ungladiador.obtenerPosicion())){//porque asumí que si ya no hay mas casillas entonces se llegó a la meta
-
-                if (!ungladiador.tieneLLave()) { //si no tiene llave
+                if (esLaUltimaPosicion(ungladiador.obtenerPosicion()) && !ungladiador.tieneLLave()) {
+                    // Se llegó a la meta sin equipamiento completo
                     System.out.println("\n----Se LLegó a la meta sin equipamiento completo----\n");
                     cantidadAMoverse = obtenerPosicionMitadCasilla();
                     ungladiador.retroceder(cantidadAMoverse);
                 }
-
             }
-
         }
     }
 
-    public boolean validarFinalizarJuego(){
-        int turno = this.turno.obtenerTurno(); //violando ocultamiento de la informacion(?
-        boolean devolver = false; // inicizalizamos el valor de un boleano
-        if (turno >= 30){
-            System.out.println("Se finalizo el juego ----> cantidad de turnos para llegar a 30 es " + (30-turno));
-            devolver = true;
+    public boolean validarFinalizarJuego() {
+        int turno = this.turno.obtenerTurno(); // Violando ocultamiento de la información(?
+        if (turno >= 30) {
+            System.out.println("Se finalizó el juego ----> cantidad de turnos para llegar a 30 es " + (30 - turno));
+            return true;
+        } else {
+            System.out.println("Aún NO se finalizó el juego ----> cantidad de turnos para llegar a 30 es " + (30 - turno));
+            return false;
         }
-        if (turno < 30){
-            System.out.println("Aun NO se finalizo el juego ----> cantidad de turnos para llegar a 30 es " + (30-turno));
-            devolver = false;
-        }
-        return devolver;
     }
-
 }
 
