aa6c9ea9bc64989ad2f88cde5210993f61c01a76
Isabella <avaldivia@fi.uba.ar>
2023-11-24 18:52:23 -0300

    Actualizacion de la clase Casilla

--- src/main/java/edu/fiuba/algo3/Modelo/Casillas/Casilla.java
+++ src/main/java/edu/fiuba/algo3/Modelo/Casillas/Casilla.java
@@ -4,16 +4,53 @@ import edu.fiuba.algo3.Modelo.Gladiador;
 
 public class Casilla {
     Ocupacion unaOcupacion;
+    VisitorDeCasillas visitor;
 
     public Casilla(Ocupacion ocupacion) {
         this.unaOcupacion = ocupacion;
+        this.visitor = new OperacionVisitorDeCasillas();
 
     }
 
-    public Gladiador interactuarConLaOcupacion(VisitorDeCasillas visitor, Gladiador ungladiador) {
 
-        return unaOcupacion.aceptarVisitante(visitor, ungladiador);
+    public Gladiador siguiente(Gladiador ungladiador, int cantidadAMoverse, int tamanioLista) {
 
+        ungladiador.aumentarEnergiaAlIniciarElTurno();
+        ungladiador.avanzar(cantidadAMoverse);
+        ungladiador = unaOcupacion.aceptarVisitante(visitor, ungladiador);//Patron visitor
+        estaEnLaUltimaPosicion(ungladiador,tamanioLista);
+        return ungladiador;
     }
+
+
+    private void estaEnLaUltimaPosicion(Gladiador ungladiador, int tamanioLista ) {
+
+        //porque se asume que si ya no hay mas casillas entonces se llegó a la meta
+        if (esLaUltimaPosicion(ungladiador.obtenerPosicion(), tamanioLista)) {
+            if (!ungladiador.tieneLLave()) {
+                System.out.println("\n----Se LLegó a la meta sin equipamiento completo----\n");
+                int cantidadAMoverse = obtenerPosicionMitadCasilla(tamanioLista);
+                ungladiador.retroceder(cantidadAMoverse);
+            }
+        }
+    }
+    private boolean esLaUltimaPosicion(int posicion, int tamanioLista) {
+        return  (posicion == tamanioLista - 1);
+    }
+
+    private int obtenerPosicionMitadCasilla(int tamanioLista) {
+
+        if (tamanioLista > 0) {
+            int indiceMitad = tamanioLista / 2;
+            System.out.println("Cantidad casillas a retroceder: " + indiceMitad);
+            return indiceMitad ;
+
+        } else {
+            return 0;
+        }
+    }
+
+
+
 }
 
--- src/main/java/edu/fiuba/algo3/Modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/Modelo/Tablero.java
@@ -1,9 +1,6 @@
 package edu.fiuba.algo3.Modelo;
 
 import edu.fiuba.algo3.Modelo.Casillas.Casilla;
-import edu.fiuba.algo3.Modelo.Casillas.OperacionVisitorDeCasillas;
-import edu.fiuba.algo3.Modelo.Casillas.VisitorDeCasillas;
-
 import java.util.ArrayList;
 
 public class Tablero {
@@ -32,54 +29,20 @@ public class Tablero {
 
     public boolean validarTurno(Gladiador unGladiador){return (turno.jugar(unGladiador));}
 
-    private int obtenerPosicionMitadCasilla() {
-
-        int longitud = listaDeCasillas.size();
-        if (longitud > 0) {
-            int indiceMitad = longitud / 2;
-            System.out.println("Cantidad casillas a retroceder: " + indiceMitad);
-            return indiceMitad ;
-
-        } else {
-            return 0;
-        }
-    }
-    private boolean hayMasCasillas() {
-        return (listaDeCasillas.size() > 1);
-    }
-
-    private boolean esLaUltimaPosicion(int posicion) {
-        return  (posicion == listaDeCasillas.size() - 1);
-    }
 
     public void avanzar(Dado dado) {
 
         int cantidadAMoverse = dado.tirar();
         Gladiador ungladiador = turno.siguienteTurno(listaDeGladiadores);
 
-        if (this.validarTurno(ungladiador)) {//refactorizar
-            ungladiador.aumentarEnergiaAlIniciarElTurno();
+        if (this.validarTurno(ungladiador)) {
 
-            if(hayMasCasillas()) {
+            controladorCantidadMoverse = controladorCantidadMoverse + cantidadAMoverse;
+            Casilla casillaActual = listaDeCasillas.get(controladorCantidadMoverse);
+            ungladiador = casillaActual.siguiente(ungladiador, cantidadAMoverse, listaDeCasillas.size());
 
-                ungladiador.avanzar(cantidadAMoverse);
-                //Patron visitor
-                VisitorDeCasillas visitor = new OperacionVisitorDeCasillas();
-                controladorCantidadMoverse = controladorCantidadMoverse + cantidadAMoverse;
-                Casilla casillaActual = listaDeCasillas.get(controladorCantidadMoverse);
-                ungladiador = casillaActual.interactuarConLaOcupacion(visitor, ungladiador);
+            //hay que actualizar la lista de gladiadores para despues por eso esta ungladiador con color gris
 
-            }
-
-            if(esLaUltimaPosicion(ungladiador.obtenerPosicion())){//porque asumí que si ya no hay mas casillas entonces se llegó a la meta
-
-                if (!ungladiador.tieneLLave()) { //si no tiene llave
-                    System.out.println("\n----Se LLegó a la meta sin equipamiento completo----\n");
-                    cantidadAMoverse = obtenerPosicionMitadCasilla();
-                    ungladiador.retroceder(cantidadAMoverse);
-                }
-
-            }
 
         }
     }
