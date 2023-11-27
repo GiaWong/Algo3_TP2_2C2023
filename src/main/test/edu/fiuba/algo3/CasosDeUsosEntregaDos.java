c9e709703a20dda887faad4ba4cc43a3ace70497
Isabella <avaldivia@fi.uba.ar>
2023-11-27 12:16:45 -0300

    Test 14 pasa

--- src/main/test/edu/fiuba/algo3/CasosDeUsosEntregaDos.java
+++ src/main/test/edu/fiuba/algo3/CasosDeUsosEntregaDos.java
@@ -3,9 +3,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.io.File;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class CasosDeUsosEntregaDos {
 
@@ -52,13 +55,71 @@ public class CasosDeUsosEntregaDos {
 
         } catch (IOException e) {
             e.printStackTrace();
+            fail("Error al leer el archivo JSON");
         }
 
     }
 
     @Test
-    public void Test014SeVerificaJsonDeObstaculosYPremiosSonValidos() {
+    public void Test014SeVerificaJsonDeObstaculosYPremiosSonValidos() {//verifico el contenido de la ocupacion
+
+
+        String rutaDelArchivo = "src/main/java/edu/fiuba/algo3/Modelo/Mapa/mapa.json";
+
+        try {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(new File(rutaDelArchivo));
+            JsonNode celdas = jsonNode.get("camino").get("celdas");
+
+            // Para almacenar obstáculos y premios extraidos del json
+            List<String> listaObstaculosJSON = new ArrayList<>();
+            List<String> listaPremiosJSON = new ArrayList<>();
+
+            // Itero sobre las celdass del camino
+            for (JsonNode celda : celdas) {
+
+                String obstaculo = celda.get("obstaculo").asText().toUpperCase();
+                String premio = celda.get("premio").asText().toUpperCase();
+                listaObstaculosJSON.add(obstaculo);
+                listaPremiosJSON.add(premio);
+
+            }
+
+            assertTrue(existenObstaculosNoContemplados(listaObstaculosJSON), "Existen obstáculos invalidos");
+            assertTrue(existenPremiosNoContemplados(listaPremiosJSON), "Existen premios invalidos");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Error al leer el archivo JSON");
+        }
+
     }
+
+    private boolean existenObstaculosNoContemplados(List<String> listaObstaculosJSON) {
+
+        List<String> obstaculosPermitidos = new ArrayList<>();
+        obstaculosPermitidos.add("".toUpperCase());
+        obstaculosPermitidos.add("Lesion".toUpperCase());
+        obstaculosPermitidos.add("Fiera".toUpperCase());
+        obstaculosPermitidos.add("Bacanal".toUpperCase());
+
+        return listaObstaculosJSON.stream()
+                .allMatch(elementoActual -> obstaculosPermitidos.contains(elementoActual));
+
+    }
+    private boolean existenPremiosNoContemplados(List<String> listaPremiosJSON) {
+
+        List<String> premiosPermitidos = new ArrayList<>();
+        premiosPermitidos.add("".toUpperCase());
+        premiosPermitidos.add("Equipamiento".toUpperCase());
+        premiosPermitidos.add("Comida".toUpperCase());
+
+        return listaPremiosJSON.stream()
+                .allMatch(elementoActual -> premiosPermitidos.contains(elementoActual));
+
+    }
+
 
     @Test
     public void Test015SeVerificaLaLecturayConversionDeEnemigosDeJsonEsValido() {
