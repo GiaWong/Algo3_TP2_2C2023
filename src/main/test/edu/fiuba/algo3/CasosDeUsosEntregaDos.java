7fda207737cc88f5d8a3ff7cffd9528e906c453d
franlopez1234 <frlopez@fi.uba.ar>
2023-11-27 20:34:23 -0300

    Test 16

--- src/main/test/edu/fiuba/algo3/CasosDeUsosEntregaDos.java
+++ src/main/test/edu/fiuba/algo3/CasosDeUsosEntregaDos.java
@@ -3,9 +3,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.fiuba.algo3.Modelo.Casillas.NadaOcupacion;
 import edu.fiuba.algo3.Modelo.Casillas.Ocupable;
+import edu.fiuba.algo3.Modelo.Equipamientos.PremioEquipamiento;
 import edu.fiuba.algo3.Modelo.Obstaculos.Bacanal;
 import edu.fiuba.algo3.Modelo.Obstaculos.FieraSalvaje;
 import edu.fiuba.algo3.Modelo.Obstaculos.Lesion;
+import edu.fiuba.algo3.Modelo.Premios.Comida;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -130,6 +132,61 @@ public class CasosDeUsosEntregaDos {
 
     }
 
+    private boolean conversionObstaculosJsonAlModelo(List<String> listaObstaculosJSON) {
+        List<Ocupable> listaDeObjetosObstaculos = new ArrayList<>();
+        if(existenObstaculosNoContemplados(listaObstaculosJSON)) {
+
+            for (String nombreActual : listaObstaculosJSON) {
+
+                if (nombreActual.equalsIgnoreCase("Lesion")) {
+                    System.out.println("\nConvirtiendo a clase Lesion()");
+                    listaDeObjetosObstaculos.add(new Lesion());
+
+                } else if (nombreActual.equalsIgnoreCase("Fiera")) {
+                    System.out.println("\nConvirtiendo a clase FieraSalvaje()");
+                    listaDeObjetosObstaculos.add( new FieraSalvaje(5));
+
+                } else if (nombreActual.equalsIgnoreCase("Bacanal")) {
+                    System.out.println("\nConvirtiendo a clase Bacanal()");
+                    listaDeObjetosObstaculos.add( new Bacanal());
+                } else
+                {
+                    System.out.println("\nConvirtiendo a clase NadaOcupacion()");
+                    listaDeObjetosObstaculos.add( new NadaOcupacion());
+                }
+            }
+            return true;
+        }
+        return false;
+
+    }
+    private boolean conversionDePremiosJsonAlModelo(List<String> listaPremiosJSON) {
+        List<Ocupable> listaDeObjetosPremios = new ArrayList<>();
+        if (existenPremiosNoContemplados(listaPremiosJSON)) {
+
+            for (String nombreActual : listaPremiosJSON) {
+
+                if (nombreActual.equalsIgnoreCase("Equipamiento")) {
+                    System.out.println("\nConvirtiendo a clase Equipamiento");
+                    listaDeObjetosPremios.add(new PremioEquipamiento());
+
+                } else if (nombreActual.equalsIgnoreCase("Comida")) {
+                    System.out.println("\nConvirtiendo a clase Comida");
+                    listaDeObjetosPremios.add(new Comida(15));
+
+                }
+                else
+                {
+                    System.out.println("\nConvirtiendo a clase NadaOcupacion()");
+                    listaDeObjetosPremios.add(new NadaOcupacion());
+                }
+
+            }
+            return true;
+        }
+
+        return false;
+    }
 
     @Test
     public void Test015SeVerificaLaLecturayConversionDeObstaculosDelJsonAlModelo() {//los contenidos de obstaculos vincularlos con los objetos del modelo
@@ -156,37 +213,29 @@ public class CasosDeUsosEntregaDos {
         }
 
     }
-
-    private boolean conversionObstaculosJsonAlModelo(List<String> listaObstaculosJSON) {
-        List<Ocupable> listaDeObjetosObstaculos = new ArrayList<>();
-        if(existenObstaculosNoContemplados(listaObstaculosJSON)) {
-
-            for (String nombreActual : listaObstaculosJSON) {
-
-                if (nombreActual.equalsIgnoreCase("Lesion")) {
-                    System.out.println("\nConvirtiendo a clase Lesion()");
-                     listaDeObjetosObstaculos.add(new Lesion());
-
-                } else if (nombreActual.equalsIgnoreCase("Fiera")) {
-                    System.out.println("\nConvirtiendo a clase FieraSalvaje()");
-                    listaDeObjetosObstaculos.add( new FieraSalvaje(5));
-
-                } else if (nombreActual.equalsIgnoreCase("Bacanal")) {
-                    System.out.println("\nConvirtiendo a clase Bacanal()");
-                    listaDeObjetosObstaculos.add( new Bacanal());
-                } else {
-                    System.out.println("\nConvirtiendo a clase NadaOcupacion()");
-                    listaDeObjetosObstaculos.add( new NadaOcupacion());
-                }
-            }
-            return true;
-        }
-        return false;
-
-    }
-
     @Test
-    public void Test016SeVerificaLaLecturayConversionDePremiosDelJsonAlModelo() {//los contenidos de premio vincularlos con los objetos del modelo
+    public void test016SeVerificaLaLecturayConversionDePremiosDelJsonAlModelo() {
+
+        String rutaDelArchivo = "src/main/java/edu/fiuba/algo3/Modelo/Mapa/mapa.json";
+        try {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(new File(rutaDelArchivo));
+            JsonNode celdas = jsonNode.get("camino").get("celdas");
+            List<String> listaPremiosJSON = new ArrayList<>();
+
+            for (JsonNode celda : celdas) {
+                String premio = celda.get("premio").asText().toUpperCase();
+                listaPremiosJSON.add(premio);
+            }
+
+            assertTrue(conversionDePremiosJsonAlModelo(listaPremiosJSON), "\nNo se pudo convertir a los objetos del Modelo");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Error al leer el archivo JSON");
+        }
+
     }
     /*@Test
     public void Test016SeVerificaLaLecturayConversionDeEnemigosDeJsonEsValido() {
