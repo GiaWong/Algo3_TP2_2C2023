0a20a92409bdaa0616de6b054953bd47a1c80a7c
Roy Fiorilo <rfiorilo@fi.uba.ar>
2023-11-26 22:43:25 -0300

    Iniciando con los test para la 2da entrega

---
+++ src/main/test/edu/fiuba/algo3/CasosDeUsosEnTregaDos.java
@@ -0,0 +1,37 @@
+package edu.fiuba.algo3;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class CasosDeUsosEnTregaDos {
+
+    @Test
+    public void Test013SeVerificaJsonDeMapaEsValido() {
+    }
+
+    @Test
+    public void Test014SeVerificaJsonDeObstaculosYPremiosSonValidos() {
+    }
+
+    @Test
+    public void Test015SeVerificaLaLecturayConversionDeEnemigosDeJsonEsValido() {
+    }
+
+    @Test
+    public void Test016SeVerificaLaLecturayConversionDeEnemigosDeJsonEsValido() {
+    }
+
+    @Test
+    public void Test017SeVerificaElsistemaLog() {
+    }
+
+    @Test
+    public void Test018SeVerificaLaCreacionDeAmbosJson() {
+    }
+
+    @Test
+    public void Test19SimulamosYVerificamosQueElJugadorGane() {
+    }
+
+    @Test
+    public void Test20SimulamosYVerificamosQueElJugadorPierda() {
+    }
+}
