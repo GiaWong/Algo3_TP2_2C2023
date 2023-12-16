8b652de6beb5cb8db8e63da5f7a96e8157930e3b
Ignacio <iurbieta@fi.uba.ar>
2023-12-15 22:48:04 -0300

    Agregado texto de la situacion de jugador

--- src/main/java/edu/fiuba/algo3/Vista/PantallaMapa.java
+++ src/main/java/edu/fiuba/algo3/Vista/PantallaMapa.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.util.ArrayList;
@@ -41,6 +42,14 @@ public class PantallaMapa extends BorderPane implements Observable {
         Stage stageMapa = new Stage();
         stageMapa.setTitle("Gladiadores en fuga");
 
+        Label textoSituacion = new Label("Bienvenidos a 'Gladiadores en fuga'");
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+
+        VBox vboxDerecha = new VBox();
+        vboxDerecha.setAlignment(Pos.CENTER);
+        Label labelHola = new Label("Hola");
+        vboxDerecha.getChildren().add(labelHola);
 
         GridPane gridMapa = new GridPane();
         gridMapa.setPadding(new Insets(10));
@@ -49,6 +58,8 @@ public class PantallaMapa extends BorderPane implements Observable {
         int ancho = 10;
         int largo = 18;
         double tamanoCasilla = 40.0;
+
+        this.setRight(vboxDerecha);
 
         for (int i = ancho; i >= 1; i--) {
             for (int j = 1; j <= largo; j++) {
@@ -68,16 +79,13 @@ public class PantallaMapa extends BorderPane implements Observable {
         Jugadores jugadores = new Jugadores(gladiadores,gridMapa);
         jugadores.actualizar();
 
-
-
         Button btnAvanzar = new Button("Tirar dado");
         btnAvanzar.setOnAction(e -> {
-
             tablero.avanzar();
             jugadores.actualizar();
-
         });
 
+        vbox.getChildren().addAll(textoSituacion, gridMapa, btnAvanzar);
 
         gridMapa.add(btnAvanzar, 0, ancho + 1, largo, 1);
         gridMapa.setAlignment(Pos.CENTER);
@@ -85,7 +93,7 @@ public class PantallaMapa extends BorderPane implements Observable {
         double ventanaAncho = largo * tamanoCasilla + 15;
         double ventanaAlto = ancho * tamanoCasilla + 15;
 
-        Scene sceneMapa = new Scene(gridMapa, ventanaAncho, ventanaAlto);
+        Scene sceneMapa = new Scene(vbox, ventanaAncho, ventanaAlto);
 
         stageMapa.setScene(sceneMapa);
         stageMapa.setMaximized(true);
